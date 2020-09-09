4aed1e8e0de2f44e59a77e561497536986adac86
William Suh <43299164+toki1@users.noreply.github.com>
2020-09-09 00:10:20 -0500

    enemy attack and movement

---
+++ Assets/gameplayManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int tick = 0;
+    public void playerTick()
+    {
+
+    }
+    
+}
