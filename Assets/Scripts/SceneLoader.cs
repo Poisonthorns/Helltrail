3fda1874c7f98f895168e66bda23e25fcb2c443e
Poisonthorns <evanssm@utexas.edu>
2020-09-08 23:27:45 -0500

    Fixed SceneLoader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneMangement;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     void GoToMenu()
     {
