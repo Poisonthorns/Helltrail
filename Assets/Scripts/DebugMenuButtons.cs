56733c2f0ac4a08410b4a932dc061d5a3317526c
presleyhoyl <presleyhoyl@gmail.com>
2020-09-18 00:39:34 -0500

    Added Debug Menu

---
+++ Assets/Scripts/DebugMenuButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DebugMenuButtons : MonoBehaviour
+{
+    public void GoToScene()
+    {
+        string sceneName = gameObject.name;
+        SceneManager.LoadScene(sceneName);
+    }
+}
