01d71e7c8b0f79eecb16926a2bb944e7dc345445
presleyhoyl <presleyhoyl@gmail.com>
2020-09-23 00:47:12 -0500

    Added Completed Music for Each Circle and Wrote AudioManager Script

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,10 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private AudioManager audioManager;
+    [SerializeField]
+    private AudioClip menuSong;
+    [SerializeField]
+    private AudioClip limboSong;
+    [SerializeField]
+    private AudioClip gluttonySong;
+    [SerializeField]
+    private AudioClip violenceSong;
+    [SerializeField]
+    private AudioClip heresySong;
+    [SerializeField]
+    private AudioClip treacherySong;
+
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     public void GoToMenu()
     {
         //Insert correct number
+        audioManager.PlayMusicWithFade(menuSong, 0.1f);
         SceneManager.LoadScene(0);
+    }
+
+    public void GoToGluttony()
+    {
+        //Insert correct number
+        audioManager.PlayMusicWithFade(gluttonySong, 0.1f);
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToViolence()
+    {
+        //Insert correct number
+        audioManager.PlayMusicWithFade(violenceSong, 0.1f);
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToHeresy()
+    {
+        //Insert correct number
+        audioManager.PlayMusicWithFade(heresySong, 0.1f);
+        SceneManager.LoadScene(4);
+    }
+
+    public void GoToTreachery()
+    {
+        //Insert correct number
+        audioManager.PlayMusicWithFade(treacherySong, 0.1f);
+        SceneManager.LoadScene(5);
+    }
+
+    public void GoToWinScreen()
+    {
+       // SceneManager.LoadScene(2);
     }
 
     public void GoToDeathScreen()
@@ -20,6 +72,7 @@ public class SceneLoader : MonoBehaviour
     public void GoToGame()
 	{
         //Insert correct number
+        audioManager.PlayMusicWithFade(limboSong, 0.1f);
         SceneManager.LoadScene(1);
     }
 }
