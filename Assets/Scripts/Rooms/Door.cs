9636c855a21528cd5e2d9daffe1c56a9699874d6
williamsuh123 <43299164+toki1@users.noreply.github.com>
2020-10-14 13:15:59 -0500

    beta stage room generation

---
+++ Assets/Scripts/Rooms/Door.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int direction;
+    public bool isExit;
+    GameObject roomManager;
+    static bool switchh = true;
+    void Start()
+    {
+        roomManager = GameObject.Find("RoomManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject.name.Equals("Sprite"))
+        {
+            print("yay");
+            if(switchh)
+            {
+                roomManager.GetComponent<CircleCreation>().changeRooms(isExit);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name.Equals("Sprite"))
+        {
+            //print("yay");
+            if (switchh)
+            {
+                switchh = false;
+            }
+            else
+            {
+                switchh = true;
+            }
+        }
+    }
+}
