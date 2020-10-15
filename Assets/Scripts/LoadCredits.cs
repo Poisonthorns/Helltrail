870a2196542e19be57a7fe35c2a998126cf2753d
presleyhoyl <presleyhoyl@gmail.com>
2020-10-15 16:02:50 -0500

    Added Credits That Load After Win Screen

---
+++ Assets/Scripts/LoadCredits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCredits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(delayCredits());
+    }
+
+ IEnumerator delayCredits()
+ {
+        yield return new WaitForSeconds(20);
+        SceneManager.LoadScene("Credits Screen");
+ }
+}
