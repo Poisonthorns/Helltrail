a64cacee79a4e9a0822d8957974f7ea1f551df5f
Presley Hoyl <61161791+presleyhoyl@users.noreply.github.com>
2020-11-17 21:31:33 -0600

    Blood Puddles Disappear After 4 Seconds (I can adjust timing if needed)

--- Assets/Scripts/DestroyParticle.cs
+++ Assets/Scripts/DestroyParticle.cs
@@ -7,13 +7,7 @@ public class DestroyParticle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         // Destroy after 2 seconds
-        Destroy(gameObject, 2f);
+        Destroy(gameObject, 4f);
     }
 }
